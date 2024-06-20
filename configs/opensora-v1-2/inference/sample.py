0f907da4d2918c6dc3978983d07da91553e86809
root <root@notebook-b9b87d8c-875e-4907-97d6-8881eb440f40-0.notebook-b9b87d8c-875e-4907-97d6-8881eb440f40.colossal-ai.svc.cluster.local>
2024-06-20 09:53:42 +0000

    config with luchen cloud mirror

--- configs/opensora-v1-2/inference/sample.py
+++ configs/opensora-v1-2/inference/sample.py
@@ -15,20 +15,20 @@ align = 5
 
 model = dict(
     type="STDiT3-XL/2",
-    from_pretrained="hpcai-tech/OpenSora-STDiT-v3",
+    from_pretrained="/root/commonData/OpenSora-STDiT-v3",
     qk_norm=True,
     enable_flash_attn=True,
     enable_layernorm_kernel=True,
 )
 vae = dict(
     type="OpenSoraVAE_V1_2",
-    from_pretrained="hpcai-tech/OpenSora-VAE-v1.2",
+    from_pretrained="/root/commonData/OpenSora-VAE-v1.2",
     micro_frame_size=17,
     micro_batch_size=4,
 )
 text_encoder = dict(
     type="t5",
-    from_pretrained="DeepFloyd/t5-v1_1-xxl",
+    from_pretrained="/root/commonData/t5-v1_1-xxl",
     model_max_length=300,
 )
 scheduler = dict(
