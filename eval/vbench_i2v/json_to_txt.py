407cd0d5bc8a53544cd1011f706cbf6c68833b17
Shen-Chenhui <shen_chenhui@u.nus.edu>
2024-06-26 03:09:50 +0000

    fix vbench_i2v path

--- eval/vbench_i2v/json_to_txt.py
+++ eval/vbench_i2v/json_to_txt.py
@@ -3,7 +3,7 @@ import os
 
 RESOLUTIONS = ["1-1", "16-9", "7-4", "8-5"]
 
-cache_root = "/mnt/jfs-hdd/sora/data/vbench-i2v/crop"
+cache_root = "/root/commonData/vbench-i2v/crop"
 resolution = RESOLUTIONS[0]
 json_file = "vbench2_i2v_full_info.json"
 save_path = "all_i2v.txt"
